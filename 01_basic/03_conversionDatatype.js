180f47f2b5f6bff86fe600ae10a3c5e29844d7d8
Prajyot Mahavir Havale <149645266+Prajyot-1131@users.noreply.github.com>
2025-07-14 16:22:57 +0000

    Conversion Datatype

---
+++ 01_basic/03_conversionDatatype.js
@@ -0,0 +1,16 @@
+/// converstion string into number 
+let name = "30ajkdj";  // "In JavaScript, when you use a number and a string together in an arithmetic operation, the result may be NaN (Not a Number) if the string cannot be properly converted to a number.         
+
+let ageNumber = Number(name)
+// console.log(ageNumber);
+// console.log(typeof ageNumber)
+
+// converstion  vaule  into Boolean 
+let isLogged = "Prajyot";              // 1 => true , 0=> false , "Prajyot"=> true, ""=> false
+let userlogged = Boolean(isLogged);
+// console.log(typeof userlogged)
+// console.log(userlogged);
+
+//converstion number into string
+
+
