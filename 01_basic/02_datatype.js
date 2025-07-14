0169c7f89f0938f23d97ecbe46d1598b25bb6807
Prajyot Mahavir Havale <149645266+Prajyot-1131@users.noreply.github.com>
2025-07-14 15:57:16 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,12 @@
+let  age = 30 // Number
+let Name="Prajyot" // String 
+ 
+let islogged = true   // Boolean
+
+let state ; // undefined
+
+let tempture = null; // object
+
+console.table([age, Name, islogged, state, tempture])
+console.log(typeof tempture )
+console.log(typeof state)
